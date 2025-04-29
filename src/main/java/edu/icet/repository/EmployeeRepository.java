6881b4cfabcc4e43fc905f4b215ad86ba8028e5d
Thenujana <chenarathenujana@gmail.com>
2025-04-29 11:15:34 +0530

    Employee repository and the Employee service impl

--- src/main/java/edu/icet/repository/EmployeeRepository.java
+++ src/main/java/edu/icet/repository/EmployeeRepository.java
@@ -1,10 +1,12 @@
 package edu.icet.repository;
 
 import edu.icet.dto.Employee;
+import edu.icet.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
     List<Employee> findByName(String name);
+    Object findAllById(Long id);
 }
--- src/main/java/edu/icet/service/impl/EmployeeServiceImpl.java
+++ src/main/java/edu/icet/service/impl/EmployeeServiceImpl.java
@@ -1,39 +1,58 @@
 package edu.icet.service.impl;
 
 import edu.icet.dto.Employee;
+import edu.icet.entity.EmployeeEntity;
+import edu.icet.repository.EmployeeRepository;
 import edu.icet.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
+@RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+ final ModelMapper mapper;
+ final EmployeeRepository repository;
 
     @Override
     public void addEmployee(Employee employee) {
-
+       repository.save(mapper.map(employee, EmployeeEntity.class));
     }
 
     @Override
     public List<Employee> getAll() {
-        return List.of();
+        List<Employee>employeeList=new ArrayList<>();
+        List<EmployeeEntity>all=repository.findAll();
+        all.forEach(employeeEntity -> {
+            employeeList.add(mapper.map(employeeEntity,Employee.class));
+        });
+        return employeeList;
     }
 
     @Override
     public void updateEmployee(Employee employee) {
-
+     repository.save(mapper.map(employee,EmployeeEntity.class));
     }
 
     @Override
     public void deleteEmployee(Long id) {
-
+      repository.deleteById(id);
     }
 
     @Override
     public Employee searchById(Long id) {
-        return null;
+       return mapper.map(repository.findAllById(id),Employee.class);
     }
 
     @Override
     public List<Employee> searchByName(String name) {
-        return List.of();
+        List<Employee> employeeEntities = repository.findByName(name);
+        return employeeEntities.stream()
+                .map(employeeEntity -> mapper.map(employeeEntity, Employee.class))
+                .collect(Collectors.toList());
     }
 }
